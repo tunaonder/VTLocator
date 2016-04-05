1df87064eece65ca690543445e893d201ea4ca62
Mike <derek22@vt.edu>
2016-04-05 11:32:43 -0400

    Created account manager file

---
+++ src/java/com/vtlocator/managers/AccountManager.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Michael (Derek) Steele on 2016.04.05  * 
+ * Copyright Â© 2016 Michael Steele. All rights reserved. * 
+ */
+package com.vtlocator.managers;
+
+/**
+ *
+ * @author Michael
+ */
+public class AccountManager {
+    
+}
