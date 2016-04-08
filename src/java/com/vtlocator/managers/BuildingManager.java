2a7277a78f66b562ad5815bc8649c6f2e350a038
tunaonder <saito@vt.edu>
2016-04-08 10:39:19 -0400

    to building 1

--- src/java/com/vtlocator/managers/BuildingManager.java
+++ src/java/com/vtlocator/managers/BuildingManager.java
@@ -7,7 +7,12 @@ package com.vtlocator.managers;
 import com.vtlocator.jpaentityclasses.Building;
 import com.vtlocator.sessionbeans.BuildingFacade;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
@@ -30,45 +35,25 @@ public class BuildingManager implements Serializable{
     @EJB
     private com.vtlocator.sessionbeans.BuildingFacade ejbFacade;
     private List<Building> items = null;
-    private Building selected;
-    
-     private String size;
+    private String selectedBuildingName;
 
-    //These HashMaps are used to have price information for all specific sizes
-    private Map<String, String> sizes;
-    
+    //Building Names Hash Map to Display at Building Menu
+    private HashMap<String, String> buildingNames;
     
     @PostConstruct
     public void init() {
         items = getItems();
-        selected = items.get(0);
-        System.out.println(selected.getName());
         
        //Sizes are in a  Hash Map
-        sizes = new HashMap<>();
-        sizes.put("Small", "Small");
-        sizes.put("Medium", "Medium");
-        sizes.put("Large", "Large");
+        buildingNames = new HashMap<>();
+        for (int i = 0; i<items.size(); i++){
+            buildingNames.put(items.get(i).getName(), items.get(i).getName());
+        }
+        buildingNames = sortByValues(buildingNames);
         
-        size = sizes.get("Small");
+        selectedBuildingName = items.get(0).getName();
 
     
-    }
-    
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public Map<String, String> getSizes() {
-        return sizes;
-    }
-
-    public void setSizes(Map<String, String> sizes) {
-        this.sizes = sizes;
     }
     
     //Calls companyFacade and gets items from the database
@@ -83,18 +68,49 @@ public class BuildingManager implements Serializable{
         return ejbFacade;
     }
 
-    public Building getSelected() {
-        return selected;
+    public String getSelectedBuildingName() {
+        return selectedBuildingName;
     }
 
-    public void setSelected(Building selected) {
-        this.selected = selected;
+    public void setSelectedBuildingName(String selectedBuildingName) {
+        this.selectedBuildingName = selectedBuildingName;
+    }
+
+    public HashMap<String, String> getBuildingNames() {
+        return buildingNames;
+    }
+
+    public void setBuildingNames(HashMap<String, String> buildingNames) {
+        this.buildingNames = buildingNames;
     }
     
     
-    
-    
+    //Sort Buildings Alphabetacially
+     private static HashMap sortByValues(HashMap map) { 
+       List list = new LinkedList(map.entrySet());
+       // Defined Custom Comparator here
+       Collections.sort(list, (Object o1, Object o2) -> ((Comparable) ((Map.Entry) (o1)).getValue())
+               .compareTo(((Map.Entry) (o2)).getValue()));
+
+       HashMap sortedHashMap = new LinkedHashMap();
+       for (Iterator it = list.iterator(); it.hasNext();) {
+              Map.Entry entry = (Map.Entry) it.next();
+              sortedHashMap.put(entry.getKey(), entry.getValue());
+       } 
+       return sortedHashMap;
+  }
+     
+  public void displayBuildingLocation(){
+     
+      
+      
+  }   
 }
+    
+    
+    
+    
+    
 
 
     
