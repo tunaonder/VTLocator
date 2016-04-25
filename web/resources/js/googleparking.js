f0a82dab49188ecd9b71d9a27d070675dec176b6
jm199seo <jm199seo@vt.edu>
2016-04-25 18:10:56 -0400

    Implementation of update/edit item. And also fixed cancel button on createItem page to delete previous entries.

--- web/resources/js/googleparking.js
+++ web/resources/js/googleparking.js
@@ -2,7 +2,7 @@
  * Created by Sean Goodrich on 2016.04.07  *
  * Copyright © 2016 Sean Goodrich. All rights reserved. *
  */
-var map; 
+var map;
 
 
 
@@ -30,7 +30,52 @@ function initLostAndFound() {
       setLostAndFoundFields(e.latLng.lng(), e.latLng.lat());
       x++;
   });
-}  
+}
+
+function initEditMap(longitude, latitude) {
+
+    // Define the LatLng coordinates for the polygon's path.
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: {
+            lat: latitude,
+            lng: longitude
+        },
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.BOTTOM_LEFT
+        },
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+
+    var latLng = new google.maps.LatLng(latitude, longitude);
+    var originalMarker = new google.maps.Marker({
+        position: latLng,
+        map: map
+    });
+
+    // marker = placeMarkerAndPanTo(e.latLng, map, marker);
+    // marker.setMap(map);
+
+    x = 0;
+    var marker;
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
+    map.addListener('click', function(e) {
+        if (originalMarker) {
+            originalMarker.setMap(null);
+        }
+        if (x>0) {
+            marker.setMap(null);
+        }
+        marker = placeMarkerAndPanTo(e.latLng, map, marker);
+        marker.setMap(map);
+        geocodeLatLng(geocoder,map, infowindow, e.latLng, marker);
+        setLostAndFoundFields(e.latLng.lng(), e.latLng.lat());
+        x++;
+    });
+}
 
 function setLostAndFoundFields(longitude, latitude) {
      document.getElementById(PF('latitude').id).value = latitude;
@@ -68,7 +113,7 @@ function geocodeLatLng(geocoder, map, infowindow, latlng, marker) {
     }
   });
 }
-      
+
 var map;
 var bounds;
 
@@ -78,7 +123,7 @@ function processShapes(input) {
     var i = 0;
     var inputLen = input.length;
     var markerBounds = new google.maps.LatLngBounds();
-    
+
     for (shapeNum in input) {
         var shapeCoords = [];
         var coordArr = input[shapeNum]["coords"];
@@ -94,7 +139,7 @@ function processShapes(input) {
                 lng: Number(coordArr[index]["coord"]["lat"]),
                 lat: Number(coordArr[index]["coord"]["long"])
             };
-            
+
             shapeCoords.push(coord);
             markerBounds.extend(new google.maps.LatLng(Number(coordArr[index]["coord"]["long"]), Number(coordArr[index]["coord"]["lat"])));
         } // end for
@@ -113,7 +158,7 @@ function processShapes(input) {
     for(var i=0; i<shapes.length; i++) {
       shapes[i].setMap(map);
     }
-    
+
     map.fitBounds(markerBounds);
 } // end function
 
@@ -138,7 +183,7 @@ function initMap() {
 }
 
 function initItem(longitude, latitude) {
-    
+
     // Define the LatLng coordinates for the polygon's path.
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
@@ -187,14 +232,14 @@ function initLostAndFoundSearch() {
       marker.setMap(lfmap);
       geocodeLatLng(geocoder,lfmap, infowindow, e.latLng, marker);
       setLocationSearchFields(e.latLng.lng(), e.latLng.lat());
-      
+
       setGeocodedAddress(geocoder, e.latLng);
       x++;
   });
-}  
+}
 
 function setGeocodedAddress(geocoder, latlng) {
-    
+
     geocoder.geocode({'location': latlng}, function(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
           if (results[1]) {
@@ -209,5 +254,5 @@ function setGeocodedAddress(geocoder, latlng) {
           document.getElementById(PF('geocodedAddress').id).value = address;
         }
      });
-    
+
 }
